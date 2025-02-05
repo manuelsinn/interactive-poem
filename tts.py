5b5098c219ec965bba9490ccc4afb5f74a2c4270
Manuel Sinn <manuel.sinnn@gmail.com>
2025-02-05 12:04:02 +0100

    initial push

---
+++ tts.py
@@ -0,0 +1,33 @@
+import torch
+from TTS.api import TTS
+
+# Get device
+device = "cuda" if torch.cuda.is_available() else "cpu"
+
+# List available 🐸TTS models
+# print(TTS().list_models())
+
+# Init TTS
+tts = TTS("tts_models/multilingual/multi-dataset/xtts_v2").to(device)
+
+
+english_file = "poem_english.txt"
+arabic_file = "poem_arabic.txt"
+turkish_file = "poem_turkish.txt"
+
+with open(english_file, "r", encoding="utf-8") as f:
+    poem_english = f.read()
+
+with open(arabic_file, "r", encoding="utf-8") as f:
+    poem_arabic = f.read()
+
+with open(turkish_file, "r", encoding="utf-8") as f:
+    poem_turkish = f.read()
+
+
+# Run TTS
+# Text to speech to a file
+speaker = "manu" # scholz
+tts.tts_to_file(text=poem_english, speaker_wav=speaker + ".wav", language="en", file_path="output_en.wav")
+tts.tts_to_file(text=poem_arabic, speaker_wav=speaker + ".wav", language="ar", file_path="output_ar.wav")
+tts.tts_to_file(text=poem_turkish, speaker_wav=speaker + ".wav", language="tr", file_path="output_tr.wav")
